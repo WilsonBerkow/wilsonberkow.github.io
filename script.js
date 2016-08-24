fe87263117d0671b57b9784a7f54902cfb283d42
Wilson Berkow <wilsonberkow@gmail.com>
2016-08-24 19:37:26 -0400

    Add head of CV to Experience (and include video of DEStiny at CRC)

---
+++ script.js
@@ -0,0 +1,14 @@
+(function () {
+    // Set the "WATCH DESTINY" button to toggle display of the
+    // video of DEStiny in autonomous.
+    var openButton = document.getElementById("cv-video-opener");
+    var videoContainer = document.getElementById("cv-video-container");
+    openButton.addEventListener("click", function (event) {
+        if (videoContainer.style.display === "block") {
+            videoContainer.style.display = "none";
+        } else {
+            videoContainer.style.display = "block";
+        }
+        event.preventDefault();
+    });
+}());
