36e3e894b6cd0d542fdc70cc75af3bc013ead808
Wilson Berkow <wilsonberkow@gmail.com>
2016-11-26 09:07:58 -0500

    Reflect whether CRC video is open in URL

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 (function () {
-    // Set the "WATCH DESTINY SCORE" button to toggle display of the
-    // video of DEStiny in autonomous.
+    // Normalize path
+    if (window.location.pathname.toLowerCase().startsWith("/watch-crc")) {
+        window.history.replaceState({view: "crc-video"}, "CRC Video", "/watch-crc");
+    }
+
+    // Utils
+    function showCRCVideo() {
+        window.history.replaceState({view: "crc-video"}, "CRC Video", "/watch-crc");
+        videoContainer.style.display = "block";
+    }
+    function hideCRCVideo() {
+        window.history.replaceState({view: "default"}, "Wilson Berkow", "/");
+        videoContainer.style.display = "none";
+    }
+    function isCRCInPath() {
+        return window.location.pathname.startsWith("/watch-crc");
+    }
+
+    // Set the "WATCH DESTINY SCORE" button to toggle display of
+    // the video of DEStiny.
     var openButton = document.getElementById("cv-video-opener");
     var videoContainer = document.getElementById("cv-video-container");
     openButton.addEventListener("click", function (event) {
-        if (videoContainer.style.display === "block") {
-            videoContainer.style.display = "none";
+        if (isCRCInPath()) {
+            hideCRCVideo();
         } else {
-            videoContainer.style.display = "block";
+            showCRCVideo();
         }
         event.preventDefault();
     });
+
+    // If user navigated directly to "/watch-crc", display the video
+    if (isCRCInPath()) {
+        showCRCVideo();
+    }
 }());
