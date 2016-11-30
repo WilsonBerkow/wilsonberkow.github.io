f7f265a7e489651c24c875e04302b9e365efe175
Wilson Berkow <wilsonberkow@gmail.com>
2016-11-29 23:09:00 -0500

    Change URL fragment for CRC-video to #auto-crc (but #watch-crc still works)

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 (function () {
     // Normalize path
-    if (window.location.hash.toLowerCase().startsWith("#watch-crc")) {
-        window.history.replaceState({view: "crc-video"}, "CRC Video", "#watch-crc");
-    }
+    (function () {
+        var lowered = window.location.hash.toLowerCase();
+        if (lowered.startsWith("#watch-crc") || lowered.startsWith("#auto-crc")) {
+            window.history.replaceState({view: "crc-video"}, "CRC Video", "#auto-crc");
+        }
+    }());
 
     // Utils
     var openButton = document.getElementById("cv-video-opener");
     var videoContainer = document.getElementById("cv-video-container");
     function showCRCVideo() {
-        window.history.replaceState({view: "crc-video"}, "CRC Video", "#watch-crc");
+        window.history.replaceState({view: "crc-video"}, "CRC Video", "#auto-crc");
         videoContainer.style.display = "block";
     }
     function hideCRCVideo() {
@@ -16,7 +19,7 @@
         videoContainer.style.display = "none";
     }
     function isCRCInURL() {
-        return window.location.hash.startsWith("#watch-crc");
+        return window.location.hash.startsWith("#auto-crc");
     }
 
     // Set the "WATCH DESTINY SCORE" button to toggle display of
@@ -30,7 +33,7 @@
         event.preventDefault();
     });
 
-    // If user navigated directly to "#watch-crc", display the video
+    // If user navigated directly to "#auto-crc", display the video
     if (isCRCInURL()) {
         showCRCVideo();
     }
